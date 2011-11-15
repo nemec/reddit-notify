e0e59bc02c08d5cf9242dd51e390aca7df3a228e
Nemec <djnemec@gmail.com>
2011-11-15 13:27:04 -0600

    Now initializes the notification system so that notifications display on Gnome3/Gnome Shell

--- reddit-notify.py
+++ reddit-notify.py
@@ -8,17 +8,24 @@ import pynotify
 import subprocess
 import urllib2
 import argparse
-import reddit
 import indicate
+import indicatoraccount
+
+# For debugging
+#import sys
+#sys.path.insert(0, "../../src/reddit_api")
+
+import reddit
 
 
 INSTALL_PATH = os.path.dirname(os.path.abspath(__file__))
 desktop_file = "%s/reddit.desktop" % INSTALL_PATH
 icon_path = "%s/reddit.png" % INSTALL_PATH
 
-class RedditAccount(object):
+class RedditAccount(indicatoraccount.IndicatorAccount):
 
   def __init__(self, username, password):
+    super(RedditAccount, self).__init__(username)
     self.api = reddit.Reddit(user_agent="reddit-notify")
     self.username = username.strip()
     self.password = password.strip()
@@ -32,11 +39,6 @@ class RedditAccount(object):
   def get_new_messages(self):
     return self.inbox.get_new_messages()
 
-  def build_indicator(self):
-    self.indicator = indicate.Indicator()
-    self.indicator.set_property("subtype", "micro")
-    self.indicator.set_property("sender", self.username)
-
 
 def get_executable_path(name):
     path = "%s/%s" % (os.getcwd(), name)
@@ -48,8 +50,8 @@ def get_executable_path(name):
     raise PathNotFound("%s not found" % name)
 
 
-class redditnotify(object):
-  """ redditnotify object
+class RedditNotify(indicatoraccount.IndicatorAccountManager):
+  """ RedditNotify object
       users: sequence of (username, password) tuples
       bother: a boolean determining whether or not to display a notification
         box upon new messages
@@ -57,12 +59,8 @@ class redditnotify(object):
         to stdout
   """
   def __init__(self, users, bother, quiet):
-    server = indicate.indicate_server_ref_default()
-    server.set_type("message.micro")
-    server.set_desktop_file(desktop_file)
-    server.connect("server-display", self.clicked)
-    server.show()
-
+    super(RedditNotify, self).__init__(desktop_file)
+    pynotify.init("Reddit Notify")
     self.bother = bother
     self.quiet = quiet
     self.unread_utc = 0
@@ -95,25 +93,15 @@ class redditnotify(object):
         # Try once more...
         account.initialize_inbox()
     
-    if not self.quiet:
-      print "Building indicator..."
     for account in self.accounts:
-      account.build_indicator()
-      account.indicator.connect("user-display", self.indicator_click)
+      self.add_account(account)
 
-  def indicator_click(self, indicator, identifier):
-    self.clicked(None, None)
-    indicator.hide()
-
-  def raise_alert(self, account, count):
-    account.indicator.set_property("count", str(count))
-    account.indicator.show()
-    account.indicator.set_property("draw-attention", "true")
 
   def notify(self, title, message):
     n = pynotify.Notification(title, message, icon_path)
     n.show()
     return n
+
 
   def check_timeout(self):
     if not self.quiet:
@@ -127,7 +115,7 @@ class redditnotify(object):
       if len(mail) > 0:
         if not self.quiet:
           print "%d new messages for %s" % (len(mail), account.username)
-        self.raise_alert(account, len(mail))
+        account.show_alert(len(mail))
         if self.bother:
           unread = [msg for msg in mail if msg.created_utc > self.unread_utc]
           if unread:
@@ -140,7 +128,7 @@ class redditnotify(object):
                             (unread[0].author, account.username),
                           unread[0].body)
       else:
-        account.indicator.hide()
+        account.hide_alert()
     return True
 
   def clicked(self, server, ident):
@@ -167,7 +155,7 @@ if __name__ == "__main__":
     f = open(args.configfile)
     data = f.readlines()
     users = [tuple(line.split(":")) for line in data]
-    r = redditnotify(users, args.bother, args.quiet)
+    r = RedditNotify(users, args.bother, args.quiet)
 
     gobject.timeout_add_seconds(args.timeout, r.check_timeout)
     r.check_timeout()
